b9df79d1f383970a237a22a88c8ec92201c98c62
Rajesh M <rajeshm71290@gmail.com>
2023-05-20 17:38:04 +0530

    Thank you

--- sample.c
+++ sample.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-  int a,b,c:
+  int a,b,c;
   printf("enter the value :");
  scanf("%d%d",&a,&b);
  c=a*b;
  printf("total:%d",c);
- return 0:
+ printf("\nThank you");
+ return 0;
 }
